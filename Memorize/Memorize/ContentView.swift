516caa42cae3917b7b63ce7c70823238c30bcf75
KedarDhere <dherek@oregonstate.edu>
2022-08-09 15:49:26 -0700

    Week 1

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Kedar Dhere on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var emojis: Array<String> = ["🚗","🚕","🚙","🚌","🚎","🏎","🚓","🚑","🚒","🚐","🛻","🚚", "🚛","🚜", "🛺", "✈️", "🚈", "🚂","🛳","🚀","🚡","🛵","🚍","🚔"]
+    
+    @State var emojiCount = 4
+    
+    var body: some View {
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
+                    ForEach(emojis[0..<emojiCount] , id: \.self, content: { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    })
+                }
+            }
+            .foregroundColor(.red)
+            
+            Spacer()
+            
+            HStack {
+                remove
+                Spacer()
+                add
+            }.font(.largeTitle)
+             .padding(.horizontal)
+            
+        }.padding()
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        }, label: {
+            Image(systemName: "minus.circle")
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+    }
+}
+
+struct CardView: View {
+    @State var isFaceUp: Bool = true
+    var content: String
+    var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 20)
+        ZStack{
+            if isFaceUp {
+                shape.fill(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }
+            else {
+                shape.fill()
+            }
+        }.onTapGesture(perform: {
+            isFaceUp = !isFaceUp
+        })
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kedar Dhere on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
