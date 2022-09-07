910f202e5ed1d4ab09dc4aeea86e092869ef548a
KedarDhere <97313818+KedarDhere@users.noreply.github.com>
2022-09-06 19:57:05 -0700

    [LAB-225] Initial commit for the memorize game (#2)

---
+++ Memorize/Memorize/Theme.swift
@@ -0,0 +1,16 @@
+//
+//  Theme.swift
+//  Memorize
+//
+//  Created by Kedar Dhere on 8/16/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Theme {
+    var name: String
+    var emojis: [String]
+    var numberOfPairOfCards: Int
+    var color: Color
+}
